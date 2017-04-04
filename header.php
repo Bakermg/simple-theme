87ccf38abc0b8da1875d4f8578e9f75e2a2a6c6e
Mike Baker <bakermg@me.com>
2017-04-04 12:50:37 -0400

    Feat: fixed the nav button aan main nav also centered the video on all views

--- header.php
+++ header.php
@@ -20,34 +20,21 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'simple-theme' ); ?></a>
-	<?php
-			if ( is_front_page() && is_home() ) : ?>
-	<?php the_header_image_tag(); ?>
-	<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'simple-theme' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	<?php else : ?>
+<header id="masthead" class="site-header" role="banner">
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'simple-theme' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-			<div class="site-branding">
+		<!-- <div class="header-hello"> -->
+		<?php the_header_image_tag(); ?>
 
-			<?php the_custom_logo(); ?>
-			<div class="site-branding-text">
-
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-
-			</div>
-		</div><!-- .site-branding -->
-
+		<!-- </div> -->
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'simple-theme' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'simple-theme' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-	<?php endif; ?>
-	<div id="content" class="site-content">
+	</div>
+
+
+
+</header><!-- #masthead -->
+<div id="content" class="site-content">
